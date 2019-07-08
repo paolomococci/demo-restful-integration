985ed49f40b227dd71ab4adf32ae99adb87b72e7
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-08 23:56:24 +0200

    Update SampleRepositoryMockMvcTests.kt

--- DemoRestfulService/src/test/kotlin/local/example/demo/repository/SampleRepositoryMockMvcTests.kt
+++ DemoRestfulService/src/test/kotlin/local/example/demo/repository/SampleRepositoryMockMvcTests.kt
@@ -83,6 +83,17 @@ class SampleRepositoryMockMvcTests {
                 .andExpect(jsonPath("$.name").value("Some Name"))
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun `delete test`() {
+        val mvcResult = mockMvc!!.perform(post("/samples").content(SAMPLE))
+                .andExpect(status().isCreated)
+                .andReturn()
+        val result = mvcResult.response.getHeader("Location")
+        mockMvc.perform(delete(result!!)).andExpect(status().isNoContent)
+        mockMvc.perform(get(result)).andExpect(status().isNotFound)
+    }
+
     companion object {
         private const val SAMPLE: String = "{\"name\":\"Sample Name\"}"
     }
