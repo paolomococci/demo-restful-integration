06ec42f053e8104dd78ab8cca285c52b671c5281
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-07-09 21:17:06 +0200

    Create build.gradle.kts

---
+++ DemoRestfulWebClient/build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.1.6.RELEASE"
+	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	kotlin("jvm") version "1.3.41"
+	kotlin("plugin.spring") version "1.3.41"
+}
+
+group = "local.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val developmentOnly by configurations.creating
+configurations {
+	runtimeClasspath {
+		extendsFrom(developmentOnly)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.8")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("com.jayway.jsonpath:json-path:2.4.0")
+    implementation("org.webjars:angularjs:1.7.7")
+	implementation("org.webjars.bower:angular-material:1.1.5")
+	implementation("org.webjars:hal-browser:ad9b865")
+    implementation("org.webjars:bootstrap:4.3.1")
+    implementation("org.webjars:jquery:3.4.1")
+    implementation("org.webjars:popper.js:1.15.0")
+    implementation("org.webjars:font-awesome:5.9.0")
+    implementation("org.webjars.npm:feather-icons:4.22.1")
+    implementation("org.webjars:chartjs:26962ce-1")
+    implementation("org.webjars:d3js:5.9.1")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
