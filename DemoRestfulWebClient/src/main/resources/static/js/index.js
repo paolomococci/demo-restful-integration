0563d2282973744aa581e52d870b708ad9097ab4
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-09-15 11:48:13 +0200

    Create index.js

---
+++ DemoRestfulWebClient/src/main/resources/static/js/index.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+    angular.module('indexApp', [
+        'ngMaterial',
+        'ngRoute',
+        'ngMessages'
+    ])
+    .directive('pmHeading', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/html/elements/pm-heading.html'
+        }
+    })
+    .directive('pmFabToolbar', function() {
+        return {
+            restrict: 'E',
+            templateUrl: '/html/elements/pm-fab-toolbar.html'
+        }
+    })
+    .controller('indexCtrl', function($scope) {
+
+    });
+})();
