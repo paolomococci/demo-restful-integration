f5715ef543db6da286ae587cc69db20a32e0c366
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-09-21 18:13:53 +0200

    Update index.js

--- DemoRestfulWebClient/src/main/resources/static/js/index.js
+++ DemoRestfulWebClient/src/main/resources/static/js/index.js
@@ -17,7 +17,7 @@
             .otherwise({redirectTo: '/help'});
         $sceDelegateProvider.resourceUrlWhitelist([
             'self',
-            'http://localhost:9090/**'
+            'http://localhost:8080/**'
         ]);
     }])
     /* directives section */
@@ -35,6 +35,13 @@
             templateUrl: '/html/elements/pm-subheading.html'
         }
     })
+    .directive('pmSection', function() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            templateUrl: '/html/elements/pm-section.html'
+        }
+    })
     .directive('pmFabToolbar', function() {
         return {
             restrict: 'E',
@@ -44,7 +51,19 @@
     /* controllers section */
     .controller('helpCtrl', ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache) {
         $scope.method = 'GET';
-        $scope.url = 'samples';
+        $scope.url = '/html/test/test.html';
+        $scope.feedback = function() {
+            $scope.code = null;
+            $scope.response = null;
+            $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+                then(function(response) {
+                    $scope.status = response.status;
+                    $scope.data = response.data;
+                }, function(response) {
+                    $scope.data = response.data || 'error: request failed';
+                    $scope.status = response.status;
+                });
+        };
     }])
     .controller('indexCtrl', ['$scope', function($scope) {
 
