f8aa60b8f14d7e8819cd7f959b2ea6b2f4da3780
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-09-24 20:52:57 +0200

    Update HelpRestController.kt

--- DemoRestfulService/src/main/kotlin/local/example/demo/controller/HelpRestController.kt
+++ DemoRestfulService/src/main/kotlin/local/example/demo/controller/HelpRestController.kt
@@ -1,4 +1,4 @@
-/**
+ /**
  *
  * Copyright 2019 paolo mococci
  *
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController
 class HelpRestController {
 
     @GetMapping(value = ["/help"])
-    @CrossOrigin
+    @CrossOrigin(origins = ["http://localhost:9090"])
     fun help(): String {
         return "Hello from help service!"
     }
